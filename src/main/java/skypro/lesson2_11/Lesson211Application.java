8b71bd70a2b65b83b1cfa1a657ee1e197fad9e9b
kirillshilov <sarataza@inbox.ru>
2022-06-02 14:38:53 +0300

    init commit

---
+++ src/main/java/skypro/lesson2_11/Lesson211Application.java
@@ -0,0 +1,13 @@
+package skypro.lesson2_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson211Application.class, args);
+    }
+
+}
